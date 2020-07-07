897d3d0f3d50f74d793d9280dcec7f8178bfa8a8
Tim Moore <timmoore226@gmail.com>
2020-07-06 20:13:06 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Timothy Moore on 7/6/20.
+//  Copyright © 2020 Timothy Moore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
